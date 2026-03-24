faf182b610aa55ff8521fdb7832bc75b086452e1
Archanahosmane <114444879+Archanahosmane@users.noreply.github.com>
2026-03-23 20:45:39 -0700

    Create weather-observation-station-11.sql

---
+++ hackerrank/basic/weather-observation-station-11.sql
@@ -0,0 +1,6 @@
+/* https://www.hackerrank.com/challenges/weather-observation-station-11/problem */
+
+SELECT DISTINCT CITY FROM STATION
+WHERE (CITY NOT LIKE 'A%' AND CITY NOT LIKE 'E%' AND CITY NOT LIKE 'I%' AND CITY NOT LIKE 'O%' AND CITY NOT LIKE 'U%' )
+OR 
+(CITY NOT LIKE '%A' AND CITY NOT LIKE '%E' AND CITY NOT LIKE '%I' AND CITY NOT LIKE '%O' AND CITY NOT LIKE '%U');
