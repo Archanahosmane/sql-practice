8b813dacbe163b6802cb60cbd97a04f274c56c5c
Archanahosmane <114444879+Archanahosmane@users.noreply.github.com>
2026-03-23 20:42:24 -0700

    Create weather-observation-station-8.sql

---
+++ hackerrank/basic/weather-observation-station-8.sql
@@ -0,0 +1,6 @@
+/* https://www.hackerrank.com/challenges/weather-observation-station-8/problem */
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE (CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%')
+AND (CITY LIKE '%A' OR CITY LIKE '%E' OR CITY LIKE '%I' OR CITY LIKE '%O' OR CITY LIKE '%U');
