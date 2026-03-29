f99f70f108e6fa93e10b5aa4167d69ea87c25694
Archanahosmane <114444879+Archanahosmane@users.noreply.github.com>
2026-03-28 17:57:09 -0700

    Create the-report.sql

---
+++ hackerrank/medium/the-report.sql
@@ -0,0 +1,16 @@
+/*  https://www.hackerrank.com/challenges/the-report/problem            */
+
+SELECT 
+    CASE 
+        WHEN G.Grade >= 8 THEN S.Name 
+        ELSE 'NULL' 
+    END AS Name,
+    G.Grade, 
+    S.Marks
+FROM Students S
+JOIN Grades G 
+  ON S.Marks BETWEEN G.Min_Mark AND G.Max_Mark
+ORDER BY 
+    G.Grade DESC, 
+    CASE WHEN G.Grade >= 8 THEN S.Name END ASC,
+    CASE WHEN G.Grade < 8 THEN S.Marks END ASC;
