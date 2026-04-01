3883f314a013d90b0b2020f5450e0bf73242a6f1
Archanahosmane <114444879+Archanahosmane@users.noreply.github.com>
2026-03-31 21:10:53 -0700

    Delete 01_total_salary.sql

--- 01_total_salary.sql
+++
@@ -1,9 +0,0 @@
---Question: Show each employee’s salary along with the total salary of all employees.
---Solution: Uses SUM() window function without grouping.
---SUM(salary) OVER () calculates the sum of salaries across all rows without grouping.
---All employees are listed, and total salary is repeated for each row.
--- Table: employees(emp_id, emp_name, salary)
-
-SELECT emp_id, emp_name, salary, 
-       SUM(salary) OVER () AS total_salary
-FROM employees;
